7567d7e8969a554fd17779179bfe8b6748f688bc
Djalma Filho <designer.djalma@gmail.com>
2023-01-21 14:32:58 -0300

    Mais atts

--- darkmode.js
+++ darkmode.js
@@ -1,27 +1,11 @@
-
-let botaoTrocaTema;
-
 function trocarTema (){
-    
-    let meuHtml;
+
     let temaAtual;
     let novoTema;
-    let precoTxt;
 
-    precoTxt = document.getElementById("produto-notebook-preco")
+    temaAtual = document.documentElement.getAttribute("data-theme");
+    novoTema = (temaAtual === "light") ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", novoTema);
 
-    meuHtml = document.documentElement
-    temaAtual = meuHtml.getAttribute("data-theme");
-    novoTema = (temaAtual === "light") ? "dark" : "light"
-    novaCor = (temaAtual === "light") ? "#FFFFFF" : "#FFFFFF"
-
-
-    precoTxt.style.color = novaCor
-    meuHtml.setAttribute("data-theme", novoTema)
 }
-
-
-botaoTrocaTema = document.getElementById("btn-troca-tema");
-botaoTrocaTema.onclick = trocarTema;
-
 
