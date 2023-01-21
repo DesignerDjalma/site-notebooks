f720a95866da042fb75206f350c4e3220e4066e7
Djalma Filho <designer.djalma@gmail.com>
2023-01-21 15:17:15 -0300

    Melhorias do Dark Mode

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,57 @@
-function trocarTema (){
+function trocarTema (btn){
 
     let temaAtual;
     let novoTema;
 
     temaAtual = document.documentElement.getAttribute("data-theme");
+    
     novoTema = (temaAtual === "light") ? "dark" : "light";
+
+    console.log("Ah")
+
+
+    if (btn.classList.value === "invert-false"){
+        console.log("True")
+        btn.classList.remove("invert-false")
+        btn.classList.add( "invert-true")
+    } else {
+        console.log("False")
+
+        btn.classList.add("invert-false")
+        btn.classList.remove( "invert-true")
+    }
+
+    document.documentElement.setAttribute("data-theme", novoTema);
+
+}
+
+function trocarTemaFancy (thisBtn){
+
+    // let btn = thisBtn;
+    let btn = document.getElementById("btn-troca-tema");
+    let temaAtual;
+    let novoTema;
+
+    temaAtual = document.documentElement.getAttribute("data-theme");
+    
+    novoTema = (temaAtual === "light") ? "dark" : "light";
+
+    console.log("Ah")
+
+
+    if (btn.classList.value === "invert-false"){
+        console.log("True")
+        btn.classList.remove("invert-false")
+        btn.classList.add( "invert-true")
+        btn.innerHTML = `<i class="gg-moon"></i>`
+    } else {
+        console.log("False")
+
+        btn.classList.add("invert-false")
+        btn.classList.remove( "invert-true")
+        btn.innerHTML = `<i class="gg-sun"></i>`
+    }
+
     document.documentElement.setAttribute("data-theme", novoTema);
 
 }
